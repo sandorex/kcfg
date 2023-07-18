b97b224bfcc7a3291728fa6157f44a1413331b64
sandorex (thorium) <rzhw3h@gmail.com>
2023-07-18 21:58:42 +0200

    Added crucial basic read/write/delete test using cmd interface
    This was long time coming and i somehow overlooked it, it helped me
    find a bug, so yay for tests

---
+++ tests/test_read_write_delete.py
@@ -0,0 +1,66 @@
+# general tests on real files using main
+
+import kcfg
+
+TEXT = """[Group 1][Group 2][Group 3]
+Key1=One
+Key2=Two
+"""
+
+def test_write(tmp_path):
+    file = tmp_path / 'write'
+    file.write_text(TEXT)
+
+    try:
+        kcfg.main(['--file', str(file), '/Group 1/Group 2/Group 3/Key2', '--write', 'Three'])
+    except SystemExit:
+        pass
+
+    assert file.read_text() == r"""[Group 1][Group 2][Group 3]
+Key1=One
+Key2=Three
+
+"""
+
+    try:
+        kcfg.main(['--file', str(file), '/Group 1/Group 2/Group 3/Key3', '--write', 'Three'])
+    except SystemExit:
+        pass
+
+        assert file.read_text() == r"""[Group 1][Group 2][Group 3]
+Key1=One
+Key2=Three
+Key3=Three
+
+"""
+
+def test_read(tmp_path, capsys):
+    file = tmp_path / 'read'
+    file.write_text(TEXT)
+
+    try:
+        kcfg.main(['--file', str(file), '/Group 1/Group 2/Group 3/Key2'])
+    except SystemExit:
+        pass
+
+    captured = capsys.readouterr()
+
+    assert captured.out == 'Two\n'
+
+    # make sure the file is not modified
+    assert file.read_text() == TEXT
+
+def test_delete(tmp_path):
+    file = tmp_path / 'delete'
+    file.write_text(TEXT)
+
+    try:
+        kcfg.main(['--file', str(file), '/Group 1/Group 2/Group 3/Key2', '--delete'])
+    except SystemExit:
+        pass
+
+    assert file.read_text() ==  """[Group 1][Group 2][Group 3]
+Key1=One
+
+"""
+
